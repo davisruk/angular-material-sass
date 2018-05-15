64437f7227e3ede6bff41d1676fe6276c13b7d0a
Richard.Davis <Richard.Davis@boots.co.uk>
2018-05-15 16:02:36 +0100

    tidied up module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppNavComponent } from './app-nav/app-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule,
-         MatButtonModule,
-         MatSidenavModule,
-         MatIconModule,
-         MatListModule,
-         MatSlideToggleModule,
-         MatFormFieldModule,
-         MatSelectModule,
-         MatMenuModule,
-        MatGridListModule,
-        MatCardModule
-       } from '@angular/material';
 import { CoreModule } from './core/core.module';
-import { ThemePickerComponent } from './theme-picker/theme-picker.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppNavComponent,
-    ThemePickerComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatSlideToggleModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatGridListModule,
-    MatMenuModule,
-    MatCardModule,
     CoreModule
   ],
   providers: [],
-  entryComponents: [ThemePickerComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,65 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from 'src/app/core/services/theme.service';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatSlideToggleModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatMenuModule,
+        MatGridListModule,
+        MatCardModule } from '@angular/material';
+import { AppNavComponent } from 'src/app/app-nav/app-nav.component';
+import { ThemePickerComponent } from 'src/app/theme-picker/theme-picker.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatCardModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   exports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatCardModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppNavComponent,
+    ThemePickerComponent
   ],
-  declarations: [],
-  providers: [ThemeService]
+  declarations: [
+    AppNavComponent,
+    ThemePickerComponent
+],
+  providers: [ThemeService],
+  entryComponents: [ThemePickerComponent]
 })
 export class CoreModule { }
